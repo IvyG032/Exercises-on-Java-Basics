a10c1dd8037c068701be8c7af495ae38c6e2e842
IvyG032 <dullahan96@gmail.com>
2026-06-29 22:10:11 +0800

    completed pset

---
+++ 10.7 search()/Search.java
@@ -0,0 +1,25 @@
+/*
+Write a method called search(), which takes an array of int and an int; and returns the array index if the array contains the given int; 
+or -1 otherwise. The method's signature is as follows:
+
+public static int search(int[] array, int key);
+Also write a test driver to test this method.
+*/
+
+public class Search {
+
+    public static int search(int[] array, int key) {
+        for (int i = 0; i < array.length; i ++){
+            if (array[i]==key) {
+                return array[i];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {14, 21, 28};
+        int key = 2;
+        System.out.println(search(array, key));
+    }
+}
